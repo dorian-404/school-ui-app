ca4b970baade1ad31b548ff93d61c2ffb659fe89
dorian-404 <mishaelwontcheu@gmail.com>
2024-02-04 19:48:08 -0600

    Initial Commit

---
+++ interfaceUICcnb/interfaceUICcnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  interfaceUICcnbApp.swift
+//  interfaceUICcnb
+//
+//  Created by user248952 on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct interfaceUICcnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
